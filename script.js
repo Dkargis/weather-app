0fe18d9148a58339f3b4efefc52c637be3920d19
Dane Kargis <dane.kargis@gmail.com>
2023-04-06 18:31:58 -0600

    I have been bad at commiting, I got stuff working I made it so then the weather will display and you can search a city

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
-var APIkeyopenweather = "18c2b5d927731b2570bf1103a61e0198"
-var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
 
-document.getElementById("City-Search").addEventListener("click", searchCity);
+var APIkeyopenweather = "18c2b5d927731b2570bf1103a61e0198"
+// var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
+
 
 
 // This is a funtion to get a five day forecast for any City searched
 function getWeather(city) {
     var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkeyopenweather;
     fetch(weatherURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < 5; i++) {
             var weather = {
-                city: data.city.name,
-                temp: data.list[0].main.temp,
-                humidity: data.list[0].main.humidity,
-                wind: data.list[0].wind.speed,
-                uv: data.list[0].main.uv,
-                icon: data.list[0].weather[0].icon,
-                date: data.list[0].dt_txt,
-                getWeather: function () {
-                    return this.city + " " + this.temp + " " + this.humidity + " " + this.wind + " " + this.uv + " " + this.icon + " " + this.date;
-                }
+            city: data.city.name,
+            temp: data.list[i].main.temp,
+            humidity: data.list[i].main.humidity,
+            wind: data.list[i].wind.speed,
+            uv: data.list[i].main.uv,
+            icon: data.list[i].weather[0].icon,
+            date: data.list[i].dt_txt,}
+            console.log(weather);
+            displayWeather(weather);
+           
+
+        }
+        console.log(data);
+        var weather = {
+            city: data.city.name,
+            temp: data.list[0].main.temp,
+            humidity: data.list[0].main.humidity,
+            wind: data.list[0].wind.speed,
+            uv: data.list[0].main.uv,
+            icon: data.list[0].weather[0].icon,
+            date: data.list[0].dt_txt,
+            getWeather: function () {
+                return this.city + " " + this.temp + " " + this.humidity + " " + this.wind + " " + this.uv + " " + this.icon + " " + this.date;
+            
             }
-        })
+        }
+        return weather;
+    })
 };
 
 // This function makes it so then the city entered in the search bar will pull up the weather for that city
 function searchCity() {
-    var city = document.getElementById("City-Search").value;
+    var city = document.getElementById("city-input").value;
+    console.log("Searching for city: " + city);
     getWeather(city);
     console.log(city);
 }   
+var submitBTN = document.getElementById("submit-btn");
+submitBTN.addEventListener("click", searchCity);
 
 // This funtion makes it so the button will create elements to display the weather for the city searched in the search bar in the Five Day Forecast section
-function displayWeather() {
-    var city = document.getElementById("City-Search").value;
-    var weather = getWeather(city);
+function displayWeather(weather) {
     var weatherDiv = document.createElement("div");
     var weatherCity = document.createElement("h2");
     var weatherTemp = document.createElement("p");
